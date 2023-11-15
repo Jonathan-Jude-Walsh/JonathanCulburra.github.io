4f97535d2467f8e7768f72c5ab092f7c56380ba7
Jonathan <108246647+JonathanCulburra@users.noreply.github.com>
2023-11-15 20:18:20 +1100

    Delete index_files directory

--- index_files/js/script.js
+++
@@ -1,6 +0,0 @@
-function acceptjesus() {
-   document.querySelector(".RELIGIONALERT").classList.add("invisible");
-   setTimeout(() => {
-      document.querySelector(".RELIGIONALERT").classList.add("intangible");
-   }, 300);
-}
