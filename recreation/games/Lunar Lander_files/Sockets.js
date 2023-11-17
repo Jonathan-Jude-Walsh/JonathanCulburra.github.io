c86ae7e4e86fdcec678b190008f6ffc7a3cae4ae
Jonathan <108246647+JonathanCulburra@users.noreply.github.com>
2023-11-17 11:27:01 +1100

    Lunar Lander to function without external server

--- recreation/games/Lunar Lander_files/Sockets.js
+++ recreation/games/Lunar Lander_files/Sockets.js
@@ -1,98 +1,61 @@
-var ws, 
-	wsID, 
-	wsConnected = false, 
-	players = {},
-	connectionRetryTimeout = 1000; 
-	
+var wsID,
+    wsConnected = false,
+    players = {},
+    connectionRetryTimeout = 1000;
 
-	function initWebSocket() { 
+function initWebSocket() {
+    // For local testing, you can use a WebSocket server running on localhost
+    var localWebSocketURL = "ws://localhost:8001";
 
-		if(WEB_SOCKET_URL!="") { 
-			ws = new WebSocket("ws://"+WEB_SOCKET_URL); 
-			console.log('Attempting connection '+WEB_SOCKET_URL); 
-			ws.onopen = function(e) { 
+    ws = new WebSocket(localWebSocketURL);
+    console.log('Attempting connection ' + localWebSocketURL);
 
-				console.log('Connected to '+WEB_SOCKET_URL); 
-				wsConnected = true; 
-				
-			
+    ws.onopen = function (e) {
+        console.log('Connected to ' + localWebSocketURL);
+        wsConnected = true;
+        // Assuming you still want to send the location data even without an external server
+        sendLocation();
+    };
 
-			};
-			
-			// not sure we need this for the normal client... 
-			ws.onmessage = function(e) { 
-				//console.log(e.data); 
-				
-				var msg = JSON.parse(e.data); 
+    ws.onmessage = function (e) {
+        // Handle incoming messages (if needed)
+        var msg = JSON.parse(e.data);
+        if (msg.type === 'connect') {
+            wsID = msg.id;
+        }
+        // Handle other message types as needed
+    };
 
-				if(msg.type=='connect') { 
-					wsID = msg.id;
-					sendLocation();
+    ws.onclose = function (e) {
+        wsConnected = false;
+        console.log("disconnected from " + localWebSocketURL);
+        if (connectionRetryTimeout) {
+            setTimeout(initWebSocket, connectionRetryTimeout);
+        }
+    };
+}
 
-				} /*else if(msg.type=='join') {
-					// add new player object
-				} else if(msg.type=='update') { 
-					// update player object
-					if(!players[msg.id]) { 
-						players[msg.id] = new Lander(); 
-						players[msg.id].scale = lander.scale;
-					}
-					var player = players[msg.id]; 
-					player.pos.x = msg.x/100; 
-					player.pos.y = msg.y/100; 
-					player.rotation = msg.a; 
-					player.thrusting = (msg.t == 1);
+function sendObject(obj) {
+    // You can handle the object locally without sending it to a server
+    console.log("Local handling of object:", obj);
+}
 
-				} else if(msg.type=='leave') { 
-					// delete player object
-					if(players[msg.id]) delete players[msg.id]; 
+function sendLocation() {
+    // You can handle the location data locally without sending it to a server
+    var update = { id: wsID, type: 'location' };
 
-				}*/
+    if (typeof IP_ADDRESS !== 'undefined') {
+        update.ip = IP_ADDRESS;
+    }
+    if (typeof loc !== 'undefined') {
+        update.lat = loc.lat;
+        update.lon = loc.long;
+        update.city = loc.city;
+        update.country = loc.country;
+    }
 
-				
+    sendObject(update);
+}
 
-			};
-			ws.onclose = function(e) { 
-				wsConnected = false; 
-				console.log("disconnected from "+WEB_SOCKET_URL); 
-				if(connectionRetryTimeout) { 
-					setTimeout(initWebSocket,connectionRetryTimeout);  
-				}
-			};
-		}
-
-
-	}
-
-	function sendObject(obj) { 
-		sendSocket(JSON.stringify(obj)); 
-		
-	}
-
-	function sendSocket(msg) { 
-		if(wsConnected ) {
-			ws.send(msg); 
-		//	console.log(msg); 
-		}
-
-	}
-	function sendLocation() { 
-		var sendit = false; 
-		
-		var update = {id:wsID, type:'location'}; 
-		
-		if(typeof IP_ADDRESS!='undefined') { 
-			update.ip = IP_ADDRESS; 
-			sendit = true; 
-		}
-		if(typeof loc!='undefined') { 
-			update.lat = loc.lat; 
-			update.lon = loc.long;
-			update.city = loc.city; 
-			update.country = loc.country;
-			sendit = true; 
-		}
-		
-		if(sendit) sendObject(update);	
-		
-	}
+// Call the WebSocket initialization function
+initWebSocket();
